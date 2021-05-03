88902e06e2013ccc099ed416f68ad3034a8d87bc
Roast Gameez <70948157+OM007VK@users.noreply.github.com>
2021-05-03 16:30:43 +0530

    Update ball.js

--- ball.js
+++ ball.js
@@ -7,6 +7,7 @@ class Ball {
         }
         this.r = 10;
         this.body = Bodies.circle(x, y, this.r, options);
+        this.color=color(random(0, 255), random(0, 255), random(0,255));
         
         World.add(world, this.body);
     }
@@ -20,7 +21,7 @@ class Ball {
         rotate(angle);
         imageMode(CENTER);
         noStroke();
-        fill("red");
+        fill(this.color);
         ellipseMode(RADIUS);
         ellipse(0,0,this.r,this.r);
         pop();
